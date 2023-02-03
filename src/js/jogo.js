084789745adfa7fb12f508cf1dd71516c4e9273e
Jorge Vitor <jorgevitor571@gmail.com>
2023-02-03 11:50:15 -0300

    criando posicoes randomicas das moscas

---
+++ src/js/jogo.js
@@ -0,0 +1,34 @@
+
+var largura
+var altura
+
+function ajustaTamanhoAtual(){
+    altura = window.innerHeight
+    largura = window.innerWidth
+
+    console.log(largura, altura)
+}
+
+ajustaTamanhoAtual()
+
+function posicaoRandomica(){
+    var posicaoX = Math.floor(Math.random() * largura) - 90
+    var posicaoY = Math.floor(Math.random() * altura) - 90
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+    
+    console.log(posicaoX, posicaoY)
+    
+    var mosquito = document.createElement('img')
+    mosquito.src = './src/imagens/mosca.png'
+    mosquito.className = 'mosca1'
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+    
+    document.body.appendChild(mosquito)
+}
+
+
+
