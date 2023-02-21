82aebd4bb92b821780457bacae96493c8c349791
cwJorgeVitor <https://github.com/cwJorgeVitor>
2023-02-21 11:32:36 -0300

    Projeto final

--- src/js/jogo.js
+++ src/js/jogo.js
@@ -1,6 +1,21 @@
 
 var largura
 var altura
+var vidas = 1
+var tempo = 60
+
+var criaMosquitoTempo = 1500
+
+var nivel = window.location.search
+nivel = nivel.replace('?','')
+
+if(nivel==='normal'){
+    criaMosquitoTempo = 1500
+} else if(nivel === 'dificil'){
+    criaMosquitoTempo = 1000
+} else if(nivel === 'chucknorris'){
+    criaMosquitoTempo = 750
+}
 
 function ajustaTamanhoAtual(){
     altura = window.innerHeight
@@ -11,7 +26,36 @@ function ajustaTamanhoAtual(){
 
 ajustaTamanhoAtual()
 
+
+    var cronometro = setInterval(function(){
+
+        tempo -= 1
+        
+
+        if(tempo < 0){
+            clearInterval(cronometro)
+            clearInterval(criaMosca)
+            window.location.href = 'vitoria.html'
+        } else{
+            document.getElementById('cronometro').innerHTML = tempo
+        }
+
+    }, 1000)
+
 function posicaoRandomica(){
+
+    if(document.getElementById('mosquito')){
+        document.getElementById('mosquito').remove()
+
+        if(vidas >3){
+            window.location.href = 'fim_de_jogo.html'
+        } else{
+            document.getElementById('v' + vidas).src = "./src/imagens/coracao_vazio.png"
+
+            vidas++
+        }
+    }
+
     var posicaoX = Math.floor(Math.random() * largura) - 90
     var posicaoY = Math.floor(Math.random() * altura) - 90
 
@@ -22,12 +66,43 @@ function posicaoRandomica(){
     
     var mosquito = document.createElement('img')
     mosquito.src = './src/imagens/mosca.png'
-    mosquito.className = 'mosca1'
+    mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
     mosquito.style.left = posicaoX + 'px'
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
+    mosquito.id = 'mosquito'
+    mosquito.onclick = function(){
+        this.remove()
+    }
     
     document.body.appendChild(mosquito)
+}
+
+function tamanhoAleatorio(){
+    var classe = Math.floor(Math.random() * 3)
+
+    switch(classe){
+        case 0:
+            return 'mosca1';
+
+            case 1:
+                return 'mosca2';
+
+                case 2:
+                    return 'mosca3';
+    }
+}
+
+function ladoAleatorio(){
+    var classe = Math.floor(Math.random() * 2)
+
+    switch(classe){
+        case 0:
+            return 'ladoA';
+
+            case 1:
+                return 'ladoB'
+    }
 }
 
 
